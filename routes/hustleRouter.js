e57780ce9c0eb13e8adf85fc4d1ca9bb23023ad9
sam-knowles <thesamknowles@gmail.com>
2022-09-14 17:13:16 -0600

    added post, delete, and put functionality to routes folder

--- routes/hustleRouter.js
+++ routes/hustleRouter.js
@@ -6,18 +6,44 @@ const hustles = [
     {name: "shrub hub", description: "plant identification", market: "botanists", _id: uuid() },
     {name: "side hustles", description: "side projects", market: "entrepreneurs", _id: uuid() },
     {name: "purpose", description: "finding one's purpose", market: "humans", _id: uuid() },
-    {name: "habits", description: "habit tracking", market: "animals", _id: uuid() }
+    {name: "habits", description: "habit tracking", market: "humans", _id: uuid() }
 ]
 
 hustleRouter.get("/", (req, res) => {
     res.send(hustles)
 })
 
+hustleRouter.get("/:hustleId", (req, res) => {
+    const hustleId = req.params.hustleId
+    const foundHustle = hustles.find(hustle => hustle._id === hustleId)
+    res.send(foundHustle)
+})
+
+hustleRouter.get("/search/market", (req, res) => {
+    const market = req.query.market
+    const filteredHustles = hustles.filter(hustle => hustle.market === market)
+    res.send(filteredHustles)
+})
+
 hustleRouter.post("/", (req, res) => {
     const newHustle = req.body
     newHustle._id = uuid()
-    hustleRouter.push(newHustle)
-    res.send('Successfully added ${newHustle.name} to the database!')
+    hustles.push(newHustle)
+    res.send(`Successfully added ${newHustle.name} to the database!`)
+})
+
+hustleRouter.delete("/:hustleId", (req, res) => {
+    const hustleId = req.params.hustleId
+    const hustleIndex = hustles.findIndex(hustle => hustle._id === hustleId)
+    hustles.splice(hustleIndex, 1)
+    res.send("Successfully deleted hustle")
+})
+
+hustleRouter.put("/:hustleId", (req, res) => {
+    const hustleId = req.params.hustleId
+    const hustleIndex = hustles.findIndex(hustle => hustle._id === hustleId)
+    const updatedHustle = Object.assign(hustles[hustleIndex], req.body)
+    res.send(updatedHustle)
 })
 
 
