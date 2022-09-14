604cf74130b8b184736c761c7132a58ed0a81700
Author Name <author.name@mail.com>
2022-09-14 15:23:51 -0600

    Added models folder

--- models/hustle.js
+++ models/hustle.js
@@ -1,19 +1,19 @@
-// const mongoose = require('mongoose')
-// const Schema = mongoose.Schema
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-// const hustleSchema = new Schema({
-//     name: {
-//         type: String,
-//         required: true
-//     },
-//     description: {
-//         type: String,
-//         required: true
-//     },
-//     market: {
-//         type: String,
-//         required: true
-//     }
-// })
+const hustleSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    market: {
+        type: String,
+        required: true
+    }
+})
 
-// module.exports = mongoose.model("Hustle", hustleSchema)
+module.exports = mongoose.model("Hustle", hustleSchema)
