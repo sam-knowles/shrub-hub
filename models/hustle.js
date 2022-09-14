3e2d212be014d4f50d0667e1039484e44d0e5bd2
Sam Knowles <samuelkcknowles@gmail.com>
2022-09-14 13:12:56 -0600

    created hustle Schema

---
+++ models/hustle.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const hustleSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    market: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model("Hustle", hustleSchema)
--- server.js
+++ server.js
@@ -2,11 +2,22 @@
 // const config = require('./config');
 // const mongoose = require('mongoose');
 // const bodyParser = require('body-parser');
+// import express from 'express'
 
-import config from './config'
-import express from 'express'
+// const config = config();
+const express = require("express");
 const app = express();
 
-app.listen(config.port, () => {
-    console.log('Listening on port' + config.port);
+const shrubs = [
+    { name: "weeds", age: 420},
+    { name: "clit plant", age: 69},
+    { name: "sunflower", age: 80085}
+]
+
+app.get("/shrubs", (req, res) => {
+    res.send(shrubs);
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
 });
