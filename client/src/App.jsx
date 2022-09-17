dafb2406a4dd98810e30b268cc1f23c567979895
sam-knowles <thesamknowles@gmail.com>
2022-09-17 15:26:44 -0600

    added a form component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,24 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Hustle from './components/Hustle.jsx'
+import AddHustleForm from './components/AddHustleForm.jsx'
 
 export default function App() {
   const [hustles, setHustles] = useState([])
 
-  useEffect(() => {
+  function getHustles(){
     axios.get('/hustles')
-      .then(res => setHustle(res.data))
-      .catch(err => console.log(err))
+    .then(res => setHustle(res.data))
+    .catch(err => console.log(err))
+  }
+
+  useEffect(() => {
+    getHustles()
   }, [])
 
   return (
       <div>
+        <AddMovieForm />
         { hustles.map(hustle => <Hustle {...hustle} key={hustle.name}/>)}
     </div>
   )
---
+++ client/src/components/AddHustleForm.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+
+export default function AddHustleForm(props){
+    const initInputs = {name: "", description: "", market: ""}
+    const [inputs, setInputs] = useState(initInputs)
+
+    function handleChange(e){
+        const { name, value } = e.target
+        setInputs(prevInputs => ({...prevInputs, [name]: value}))
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+
+        setInputs(initInputs)
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+         <input 
+            type="text" 
+            name="name" 
+            value={inputs.name} 
+            onChange={handleChange} 
+            placeholder="Name"/>
+         <input type="text" 
+            name="description" 
+            value={inputs.description} 
+            onChange={handleChange} 
+            placeholder="Description"/>
+         <input 
+            type="text" 
+            name="market" 
+            value={inputs.market} 
+            onChange={handleChange} 
+            placeholder="Market"/>
+         <button>Add Hustle</button>
+        </form>
+    )
+}
